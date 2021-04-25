1cc880dd19a024b1db9ac451c98760c7d2f1262f
Buse Şentürk <buse.senturk@outlook.com>
2021-04-25 21:07:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
+/*let names = "";
+let event = "";
+function writeCards() {
+  for (let i = 0; i < names.length; i++) {
+    console.log('Thank you, ${names[i]} for the wonderful ${event[i]}  gift!');
+
+  return names;
+}
+// console.log("Thank you, " + $names[i] + " for the wonderful " + ${event} + " gift!");
+} */
+
+let names = ['Lisa', 'Kaitlin', 'Jan'];
+let event =  "surprise";
+function writeCards(names, event) {
+for (let i=0; i<names.length; i++){
+  let message = "Thank you, "+  name[i] + " for the wonderful " +  event + " gift!" ;
+  //console.log("Thank you, "+  name[i] + " for the wonderful " +  event + " gift!" )
+} return message;
+}
+//writeCards(collection);
+
+/*
+const names = ['Lisa', 'Kaitlin', 'Jan'];
+const event= "surprise";
+function writeCards(names, event) {
+for (let i = 0; i < names.length; i++) {
+   console.log(`Thank you, ${names[i]} , for the wonderful ${event} gift!`);
+ }
+}
+writeCards(names,event);
+
+*/
+
+
+
+function countDown() {
+  let numero=10;
+  while (numero>=0) {
+    console.log(numero--);
+  }
+}
+
+
+
+
+
 // Code your solutions in this file
